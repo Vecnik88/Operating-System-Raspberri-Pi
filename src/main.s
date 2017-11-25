43e39cf97b185cacce40895fbb87fd3c84cd0cd7
Vecnik <Ve_cni_k@inbox.ru>
2017-11-26 00:53:21 +0700

    add GPIO

--- src/main.s
+++ src/main.s
@@ -8,7 +8,23 @@ _start:
 	str r1, [r0, #4]
 	mov r1, #1
 	lsl r1, #16
-	str r1, [r0 #40]
 
-loop $:
-	b loop $
+loop$:
+
+	str r1, [r0, #40]
+	mov r2, #0x3f0000
+
+wait1$:
+	sub r2, #1
+	cmp r2, #0
+	bne wait1$
+
+	str r1, [r0, #28]
+	mov r2, #0x3f0000
+
+wait2$:
+	sub r2, #1
+	cmp r2, #0
+	bne wait2$
+
+b loop$
